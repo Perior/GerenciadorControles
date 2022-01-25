3d7dd3bac7db338668785589e610d7809488283a
Perior <theperior@hotmail.com>
2022-01-25 17:00:31 -0300

    BaseApplication
    Just the base project for progress tracking (before any alterations)

---
+++ manage-status/src/main/java/com/manager/managestatus/ManageStatusApplication.java
@@ -0,0 +1,13 @@
+package com.manager.managestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageStatusApplication.class, args);
+	}
+
+}
---
+++ manage-status/src/test/java/com/manager/managestatus/ManageStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.managestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
